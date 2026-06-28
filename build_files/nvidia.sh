7ff04d67ed8588b70317ebd37e183cc41fc5eab8
itzNOELdev <dev.zodium.project@gmail.com>
2026-06-28 22:59:54 +0530

    Fix : nvidia script

--- build_files/nvidia.sh
+++ build_files/nvidia.sh
@@ -10,29 +10,28 @@ dnf config-manager setopt fedora-nvidia.priority=90
 dnf makecache --refresh
 
 # Install Nvidia Kmods Drivers
-dnf install --setopt=install_weak_deps=False -y nvidia-modprobe nvidia-driver-selinux \
-    nvidia-kmod-common kmod-nvidia-"$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' | tail -1)"
+dnf install --setopt=install_weak_deps=False -y \
+    nvidia-modprobe \
+    nvidia-kmod-common \
+    kmod-nvidia-"$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' | tail -1)"
 
 # Install Nvidia Userspace Drivers
 dnf install --setopt=install_weak_deps=False -y \
     libnvidia-fbc \
     nvidia-driver \
     nvidia-libXNVCtrl \
+    nvidia-driver-cuda \
     libva-nvidia-driver \
     nvidia-persistenced \
+    nvidia-driver-common \
+    nvidia-driver-selinux \
+    nvidia-driver-cuda-libs \
     nvidia-driver-NvFBCOpenGL
 
 # Install Nvidia X11 settings
 dnf install --setopt=install_weak_deps=False -y \
-    nvidia-xconfig
-
-# Install Nvidia Cuda Toolkit
-dnf install --setopt=install_weak_deps=False -y \
-    cuda \
-    cuda-libs \
-    cuda-devel \
-    nvidia-driver-cuda \
-    nvidia-driver-cuda-libs
+    nvidia-xconfig \
+    xorg-x11-nvidia
 
 # Install Nvidia Container Toolkit
 dnf install --setopt=install_weak_deps=False -y \
@@ -44,6 +43,18 @@ dnf install --setopt=install_weak_deps=False -y \
 dnf install --setopt=install_weak_deps=False -y \
     prime-run \
     nvidia-settings
+
+# Install Nvidia Cuda Toolkit
+dnf config-manager setopt fedora-multimedia.enabled=1
+dnf config-manager setopt fedora-multimedia.priority=90
+dnf config-manager setopt fedora-nvidia.priority=80
+dnf makecache --refresh
+dnf install --setopt=install_weak_deps=False -y \
+    cuda \
+    cuda-libs \
+    cuda-devel
+
+dnf config-manager setopt fedora-multimedia.enabled=0
 
 # Nvidia Services
 systemctl enable nvidia-persistenced.service
@@ -57,3 +68,4 @@ sed -i 's/ nvidia / i915 amdgpu nvidia /g' /usr/lib/dracut/dracut.conf.d/99-nvid
 dnf config-manager setopt fedora-nvidia.enabled=0
 rm -f /etc/yum.repos.d/fedora-nvidia.repo
 dnf config-manager setopt fedora-multimedia.enabled=1
+dnf config-manager setopt fedora-multimedia.priority=90
