336617a74ff7227879ef0fd592eef1cf90a68c8e
itzNOELdev <dev.zodium.project@gmail.com>
2026-07-22 12:44:11 +0530

    Add : sanlock

--- build_files/vx-setup.sh
+++ build_files/vx-setup.sh
@@ -19,21 +19,22 @@ LIBVIRT=(
   libvirt-daemon-driver-lxc
   libvirt-daemon-driver-qemu
   libvirt-daemon-driver-secret
+  libvirt-daemon-plugin-sanlock
   libvirt-daemon-config-network
   libvirt-daemon-driver-network
   libvirt-daemon-driver-nodedev
   libvirt-daemon-config-nwfilter
   libvirt-daemon-driver-nwfilter
   libvirt-daemon-driver-interface
+  libvirt-daemon-driver-storage-rbd
   libvirt-daemon-driver-storage-zfs
   libvirt-daemon-driver-storage-core
   libvirt-daemon-driver-storage-disk
   libvirt-daemon-driver-storage-mpath
-  libvirt-daemon-driver-storage-rbd
-  libvirt-daemon-driver-storage-gluster
   libvirt-daemon-driver-storage-iscsi
-  libvirt-daemon-driver-storage-iscsi-direct
+  libvirt-daemon-driver-storage-gluster
   libvirt-daemon-driver-storage-logical
+  libvirt-daemon-driver-storage-iscsi-direct
 )
 
 QEMU=(
@@ -111,6 +112,7 @@ VIRT_HOST_DEPS=(
   swtpm
   libnbd
   nbdkit
+  sanlock
   dnsmasq
   numactl
   pciutils
